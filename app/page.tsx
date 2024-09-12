93f0f632d8dd3855f82c66e13badd3b37904ec73
Chaynes-MacBookPro <ChayneW-MacBookPro.local>
2024-09-12 11:05:15 -0400

    clean page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-// import Image from "next/image";
-// import { fetchGames } from './lib/api';
 import GamesLoad from "@/components/GamesLoad";
 import { BentoGridThirdDemo } from "@/components/Bento";
 import Hero from "@/components/Hero";
-// import { BackgroundBeamsWithCollisionDemo } from "@/components/Laser";
-
 
 // import { fetchGamesData } from '@/app/actions'; // Fetch server-side data
 
 
 export default async function Home() {
-
-
   return (
     <div className="max-md:px-10 p-20">
         <div>
